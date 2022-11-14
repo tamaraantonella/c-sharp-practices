2a6490566fc1d6d59c4fe48eadaa688b3ba9da94
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-11-14 17:00:52 -0300

    palindrome

---
+++ palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to check if it is a palindrome");
+            string input = Console.ReadLine();
+            string reverse = ReverseString(input);
+            if (input == reverse)
+            {
+                Console.WriteLine("The string is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The string is not a palindrome");
+            }
+        }
+
+        static string ReverseString(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                sb.Append(input[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
